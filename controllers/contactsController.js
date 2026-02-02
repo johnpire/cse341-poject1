454ae8c4bb417140337b3fd9637ca3a8e5b27fea
John Carlo <ijcwasbornonthe15thofseptember@gmail.com>
2026-02-02 16:05:22 +0800

    w03: polishing error handler

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -3,24 +3,25 @@ const objectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
     //#swagger.tags = ['Contacts']
-    const result = await mongodb.getDatabase().db().collection('contacts').find();
-    result.toArray().then((contacts) => {
+    mongodb.getDatabase().db().collection('contacts').find().toArray((err, contacts) => {
+        if (err) {
+            res.status(400).json({message: err});
+        }
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(contacts);
-    }).catch((err) => {
-        res.status(500).json({ message: 'Failed to retrieve contacts.', error: err });
     });
 };
 
 const getSingle = async (req, res) => {
     //#swagger.tags = ['Contacts']
     const userId = new objectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('contacts').find({ _id: userId });
-    result.toArray().then((contacts) => {
+
+    mongodb.getDatabase().db().collection('contacts').find({ _id: userId }).toArray((err, result) => {
+        if (err) {
+            res.status(400).json({message: err});
+        }
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(contacts[0]);
-    }).catch((err) => {
-        res.status(500).json({ message: 'Failed to retrieve contact.', error: err });
+        res.status(200).json(result[0]);
     });
 };
 
